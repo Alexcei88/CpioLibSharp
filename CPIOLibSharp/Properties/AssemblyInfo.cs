793bce700f33a06c5e551e5fed235fa0b20e9fa5
shmakov.a <shmakov.a@tn.ru>
2018-01-03 12:01:27 +0300

    Build new nuget package

--- CPIOLibSharp/Properties/AssemblyInfo.cs
+++ CPIOLibSharp/Properties/AssemblyInfo.cs
@@ -1,25 +1,16 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CPIOLibSharp")]
-[assembly: AssemblyDescription("Decompression library for cpio archive format")]
+[assembly: AssemblyDescription("A decompression library for cpio archive format")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CPIOLibSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © 2016-2017")]
+[assembly: AssemblyTrademark("Alexcei Shmakov")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a30c7cdd-abad-4b8c-9bb8-03bbe4cf445d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,6 +21,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2.0")]
-[assembly: AssemblyFileVersion("1.0.2.0")]
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyFileVersion("1.0.3.0")]
